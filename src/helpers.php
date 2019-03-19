bcc1029cb2facfd8af5535f81a9503b80f16fab0
Spharian <s.fertons@gmail.com>
2019-03-19 23:35:29 +0100

    Verify if flash global function exists

--- src/helpers.php
+++ src/helpers.php
@@ -3,22 +3,24 @@
 use Spatie\Flash\Flash;
 use Spatie\Flash\Message;
 
-/**
- * @param string $text
- * @param string|array $class
- */
-function flash(string $text = null, $class = null): Flash
-{
-    /** @var \Spatie\Flash\Flash $flash */
-    $flash = app(Flash::class);
+if (! function_exists('flash')) {
+    /**
+     * @param string $text
+     * @param string|array $class
+     */
+    function flash(string $text = null, $class = null): Flash
+    {
+        /** @var \Spatie\Flash\Flash $flash */
+        $flash = app(Flash::class);
 
-    if (is_null($text)) {
+        if (is_null($text)) {
+            return $flash;
+        }
+
+        $message = new Message($text, $class);
+
+        $flash->flash($message);
+
         return $flash;
     }
-
-    $message = new Message($text, $class);
-
-    $flash->flash($message);
-
-    return $flash;
 }
