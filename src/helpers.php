3d2927be8161e883eda5b029e66a6de1d70aa2db
ArshiaMohammadei <a.mmdi2007m@gmail.com>
2025-06-16 13:45:14 +0330

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -3,21 +3,37 @@
 use Spatie\Flash\Flash;
 use Spatie\Flash\Message;
 
-/**
- * @param  string|array  $class
- */
-function flash(?string $text = null, $class = null): Flash
-{
-    /** @var \Spatie\Flash\Flash $flash */
-    $flash = app(Flash::class);
+if (!function_exists('flash')) {
+    /**
+     * Flash a message to the session or return the Flash instance.
+     *
+     * @param  string|null           $text
+     * @param  string|array|null     $class
+     * @param  string|null           $level
+     * @param  array                 $meta
+     * @return \Spatie\Flash\Flash
+     */
+    function flash(
+        ?string $text = null,
+        string|array|null $class = null,
+        ?string $level = null,
+        array $meta = []
+    ): Flash {
+        /** @var \Spatie\Flash\Flash $flash */
+        $flash = app(Flash::class);
 
-    if (is_null($text)) {
+        if (is_null($text)) {
+            return $flash;
+        }
+
+        $message = Message::make($text, $class, $level);
+
+        foreach ($meta as $key => $value) {
+            $message->withMeta($key, $value);
+        }
+
+        $flash->flash($message);
+
         return $flash;
     }
-
-    $message = new Message($text, $class);
-
-    $flash->flash($message);
-
-    return $flash;
 }
